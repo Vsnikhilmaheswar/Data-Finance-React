90a69b542879f92679cfa5dfafe02eaa6754b482
Vsnikhilmaheswar <nikhilmaheswar12@gmail.com>
2025-01-25 23:59:57 +0530

    Added Cards

---
+++ src/components/Cards.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Single from "../assets/single.png";
+import double from "../assets/double.png";
+import triple from "../assets/triple.png";
+function Cards() {
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full  shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Single}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8"> Single User</h2>
+          <p className="text-center text-4xl text-bold">$149</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b border-gray-300 mx-8 mt-8 ">
+              500 GB Storage
+            </p>
+            <p className="py-2 border-b border-gray-300 mx-8  ">1 grand user</p>
+            <p className="py-2 border-b border-gray-300 mx-8 ">
+              sent up to 2gb
+            </p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto py-3 my-6 text-black">
+            Start Trail
+          </button>
+        </div>
+
+        <div className="w-full  shadow-xl bg-gray-100 flex flex-col p-4 md:m-0 my-8 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-grey-100"
+            src={double}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8"> Double User</h2>
+          <p className="text-center text-4xl text-bold">$149</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b border-gray-300 mx-8 mt-8 ">
+              500 GB Storage
+            </p>
+            <p className="py-2 border-b border-gray-300 mx-8  ">1 grand user</p>
+            <p className="py-2 border-b border-gray-300 mx-8 ">
+              sent up to 2gb
+            </p>
+          </div>
+          <button className="bg-black  w-[200px] rounded-md font-medium mx-auto py-3 my-6 text-[#00df9a]">
+            Start Trail
+          </button>
+        </div>
+
+        <div className="w-full  shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={triple}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8"> Single User</h2>
+          <p className="text-center text-4xl text-bold">$149</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b border-gray-300 mx-8 mt-8 ">
+              500 GB Storage
+            </p>
+            <p className="py-2 border-b border-gray-300 mx-8  ">1 grand user</p>
+            <p className="py-2 border-b border-gray-300 mx-8 ">
+              sent up to 2gb
+            </p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto py-3 my-6 text-black">
+            Start Trail
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Newsletter() {
   return (
-    <div className=' w-full h-50 py-16 text-white p-4 '>
+    <div className=' w-full  py-16 text-white  '>
       <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
      <div className='lg:col-span-2 my-4'>
-     <h1 className='mdtext-5xl sm:text-4xl text-xl text-bold py-2 '>Want tips & tricks optimize your flows?</h1>
+     <h1 className='md:text-5xl sm:text-4xl text-xl text-bold py-2 '>Want tips & tricks optimize your flows?</h1>
      <p>sign up to our newsletter and stay up to date.</p>
      </div>
     
@@ -14,11 +14,9 @@ function Newsletter() {
                  <input type='email' placeholder='Enter Email' className=' bg-white text-black h-12 p-3 flex  w-full rounded-md'/>
                  <button className='bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto py-3 my-6 text-black'>Get Started</button>
                 </div>
-             <p>We care about the protection of your data . Read our Privacy Policy . </p>
+             <p>We care about the protection of your data . Read our  <span className='text-[#00df9a]'>Privacy Policy .</span>{' '} </p>
            </div>
       
-       
-
       </div>
        
       </div>
